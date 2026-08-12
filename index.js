66c6b922c4a7b2f48f38c0c5b0d0892df83f9fb5
Aman Jaiswal <amankeshav2204@gmail.com>
2026-08-12 11:02:16 +0530

    LLM CALLING

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+
 dotenv.config();
 
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY
-});
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY
+// });
 
 // const main = async () => {
 //     const response = await ai.models.generateContent({
@@ -24,31 +26,51 @@ const ai = new GoogleGenAI({
 
 
 
+// app.post("/get-ans", async (req, res) => {
+//     const data = req.body;
+
+//     const response = await ai.models.generateContent({
+//         model: "gemini-3.6-flash",
+//         contents: [
+//             {
+//                 role: "user",
+//                 parts: [
+//                     {
+//                         text: data.prompt
+//                     }
+//                 ]
+//             }
+//         ],
+
+//         config: {
+//             systemInstruction:
+//                 "You are a helpful assistant and your name is David."
+//         }
+//     })
+//     console.log(response.text);
+//     res.status(200).json({
+//         message: "Request received successfully",
+//         answer: response.text
+//     });
+// })
+
+
+
+//--------LANG-CHAIN-----------------
+
+const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-3.6-flash",
+    temperature: 0,
+    maxRetries: 2,
+    // other params...
+})
 app.post("/get-ans", async (req, res) => {
     const data = req.body;
 
-    const response = await ai.models.generateContent({
-        model: "gemini-3.6-flash",
-        contents: [
-            {
-                role: "user",
-                parts: [
-                    {
-                        text: data.prompt
-                    }
-                ]
-            }
-        ],
-
-        config: {
-            systemInstruction:
-                "You are a helpful assistant and your name is David."
-        }
-    })
-    console.log(response.text);
-    res.status(200).json({
+    const response = await llm.invoke(data.prompt);
+    return res.status(200).json({
         message: "Request received successfully",
-        answer: response.text
+        answer: response.content
     });
 })
 
